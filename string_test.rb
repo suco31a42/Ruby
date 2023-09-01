8dab4442dfdc7972ec37bb3f9b047c10e056664a
suco31a42 <132245813+suco31a42@users.noreply.github.com>
2023-09-01 07:17:11 +0000

    [add]string_test.rb

--- string_test.rb
+++ string_test.rb
@@ -12,6 +12,15 @@ p "0123456789-".delete("0-28-")
 p "foo\nbar\nbaz".split(/\n/, 2)
 # chompの指定を空にすると\n, \r, \nr を全て指定して削除する
 str_chomp = "abc\r\n\r\n".chomp('')
-p str2
+p str_chomp
 # chopは末尾一文字を取り除く。ただし"\r\n"は改行コード一文字と認識する
 str_chop = "abc\r\n".chop
+
+a = "asdf"
+b = "hello"
+b = a
+# !が付くと反転した文字列を破壊的に上書きする
+b.reverse! 
+p b
+# "ber" * (2 ** 2)として先に数値が計算される
+p "ber" * 2 ** 2
